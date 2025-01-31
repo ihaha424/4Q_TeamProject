23a0818e67a7cbe3b294e34acdd45f4ed832ee22
Do_Suha <weqa123@naver.com>
2025-01-31 19:16:34 +0900

    CreateSystem 및 LogLevelToString 함수 추가
    Functions.cpp에서 #include 위치 변경
    CreateSystem 함수 추가: ISystem 포인터 유효성 검사 및 반환 값 처리
    LogLevelToString 함수 추가: LogLevel 열거형 값을 문자열로 변환
    Functions.h에 CreateSystem 및 LogLevelToString 구조체 추가

--- 4Q_Solution/DSHLogger/Functions.cpp
+++ 4Q_Solution/DSHLogger/Functions.cpp
@@ -1,2 +1,50 @@
 #include "pch.h"
 #include "Functions.h"
+
+HRESULT DSH::Logger::CreateSystem::operator()(ISystem** ppSystem) const
+{
+	if (ppSystem == nullptr) return E_INVALIDARG;
+	//System* pSystem = new System();
+	//if (pSystem == nullptr) return E_OUTOFMEMORY;
+	//*ppSystem = pSystem;
+	return S_OK;
+}
+
+DSH::Logger::String DSH::Logger::LogLevelToString::operator()(const LogLevel logLevel) const
+{
+	String string;
+	switch (logLevel)
+	{
+	case LogLevel::Trace:
+	{
+		string = LITERAL("[TRACE]");
+	}
+	break;
+	case LogLevel::Debug:
+	{
+		string = LITERAL("[DEBUG]");
+	}
+	break;
+	case LogLevel::Information:
+	{
+		string = LITERAL("[INFORMATION]");
+	}
+	break;
+	case LogLevel::Warning:
+	{
+		string = LITERAL("[WARNING]");
+	}
+	break;
+	case LogLevel::Error:
+	{
+		string = LITERAL("[ERROR]");
+	}
+	break;
+	case LogLevel::Fatal:
+	{
+		string = LITERAL("[FATAL]");
+	}
+	break;
+	}
+	return string;
+}
--- 4Q_Solution/DSHLogger/Functions.h
+++ 4Q_Solution/DSHLogger/Functions.h
@@ -4,6 +4,15 @@ namespace DSH::Logger
 {
 	EXTERN_C struct DSH_LOGGER_LIBRARY_API CreateSystem
 	{
+		HRESULT STDMETHODCALLTYPE operator()(
+			/* [annotation][out] */
+			_Out_ ISystem** ppSystem) const;
+	};
 
+	EXTERN_C struct DSH_LOGGER_LIBRARY_API LogLevelToString
+	{
+		String STDMETHODCALLTYPE operator()(
+			/* [annotation][in] */
+			_In_ LogLevel logLevel) const;
 	};
 }

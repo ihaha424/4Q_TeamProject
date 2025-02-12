42c87f9347bec2b32e5033f3aa26d9e8ba534cf8
semin0723 <45254558+semin0723@users.noreply.github.com>
2025-02-13 04:13:40 +0900

    [FEAT] 헤더 누락 수정

--- 4Q_Solution/Client/Puzzle_02_ObjectData.h
+++ 4Q_Solution/Client/Puzzle_02_ObjectData.h
@@ -1,4 +1,8 @@
 #pragma once
 
 #include "Obj_Sudium_bouquet.h"
-#include "Sudium_SoundTrigger.h"
+#include "Trigger_Sudium_SoundTrigger_1.h"
+#include "Trigger_Sudium_SoundTrigger_2.h"
+#include "Trigger_Sudium_SoundTrigger_3.h"
+#include "Trigger_Sudium_SoundTrigger_4.h"
+#include "Trigger_Sudium_SoundTrigger_5.h"

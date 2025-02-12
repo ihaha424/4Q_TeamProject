b4c4d6d35fd306881ee6cfe179221d3b50343b28
dongmiki <70957529+ihaha424@users.noreply.github.com>
2025-02-12 10:54:25 +0900

    [TEST]

--- 4Q_Solution/LogicServer/ServerLogic.cpp
+++ 4Q_Solution/LogicServer/ServerLogic.cpp
@@ -730,8 +730,8 @@ void ServerLogic::RegistPlayer(Player* player)
 {
     Engine::Physics::ControllerDesc cd;
     cd.position = Engine::Math::Vector3(0, 0, 0);
-    cd.height = 10.f;
-    cd.radius = 2.f;
+    cd.height = 50.f;
+    cd.radius = 10.f;
     cd.gravity = { 0.f, -9.8f * 10, 0.f };
     cd.contactOffset = 0.2f;
     cd.stepOffset = 10.f;
@@ -757,7 +757,7 @@ void ServerLogic::RegistGround(Ground& ground)
     _physicsManager->CreateTriangleStatic(&staticrigid, "terrain", { {0.0f,0.f,0.f } }, transform);
     ground._staticRigid = static_cast<Engine::Physics::RigidStaticComponent*>(staticrigid);
     _mainScene->AddActor(ground._staticRigid);
-    ground._staticRigid->SetTranslate({ -2560.f, -1400.f, -2560.f });
+    ground._staticRigid->SetTranslate({ -2560.f, -1350.f, -2560.f });
     //ground._staticRigid->SetTranslate({ -2560.f, -1035.f, -2560.f });
     auto tempRotation = Engine::Math::Quaternion::CreateFromYawPitchRoll(-std::numbers::pi_v<float> * 0.5f, 0.f, 0.f);
     ground._staticRigid->SetRotation(tempRotation);
@@ -766,7 +766,7 @@ void ServerLogic::RegistGround(Ground& ground)
     ground._staticRigid->Initialize();
 
     Engine::Physics::IRigidComponent* rigid;
-    _physicsManager->CreatePlane(&rigid, Engine::Math::Vector3{ 0.f,-100.f, 0 }, Engine::Math::Vector3{ 0.f,1.f, 0 }, { {0.f,0.f,0.f} });
+    _physicsManager->CreatePlane(&rigid, Engine::Math::Vector3{ 0.f,-60.f, 0 }, Engine::Math::Vector3{ 0.f,1.f, 0 }, { {0.f,0.f,0.f} });
     _mainScene->AddActor(rigid);
 }
 void ServerLogic::RegistTriggerBox(TriggerBox& triggerBox)
